8e77f01a330c1d7ee8ecc6061d4e577d6779f17f
Claes Engelin <claes.engelin@zocom.se>
2022-04-21 12:01:32 +0200

    Avbrott för lunch - litet buggigt fortfarande

---
+++ SlackClone/Domain/SlackMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackClone.Domain;
+
+public class SlackMessage
+{
+    public string PostedBy { get; set; }
+    public DateTime PostedOn { get; set; }
+    public string Message { get; set; }
+}
